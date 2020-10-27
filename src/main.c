05b32c2ddc6d86f195f6212633c6322374940ba1
Niyamaka <niyamaka@yuneta.io>
2020-10-27 23:08:56 +0100

    v4.2.20

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@
 #define APP_NAME        "emailsender"
 #define APP_DOC         "Email sender"
 
-#define APP_VERSION     "4.2.19"
+#define APP_VERSION     "4.2.20"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
