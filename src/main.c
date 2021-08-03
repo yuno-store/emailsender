b7720032b2fcab98d6f5065d7f11d0ba90722cad
Niyamaka <niyamaka@yuneta.io>
2021-08-03 21:43:16 +0200

    v4.15.7 warning agent

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@
 #define APP_NAME        "emailsender"
 #define APP_DOC         "Email sender"
 
-#define APP_VERSION     "4.15.6"
+#define APP_VERSION     "4.15.7"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
